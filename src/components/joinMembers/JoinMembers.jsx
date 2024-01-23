0db7b5853b21e2911a1f2f03c5cff13f3c7f5c95
YOUNTAEHEE <f_hyang@naver.com>
2024-01-23 09:26:41 +0900

    add

--- src/components/joinMembers/JoinMembers.jsx
+++ src/components/joinMembers/JoinMembers.jsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { addUser } from '@/lib/actions';
 //.
+//.
 export default function JoinMembers() {
 	const [state, formAction] = useFormState(addUser, undefined);
 	const router = useRouter();
